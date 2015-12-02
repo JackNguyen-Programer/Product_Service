32810cc8950d8ec73bd58d821cf1693f992ab57f
tmnhat1993 <tmnhat1993@gmail.com>
2015-12-02 23:31:14 +0700

    TaiKhoanController

---
+++ BanHang/Controllers/TaiKhoanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace BanHang.Controllers
+{
+    [Authorize]
+
+    public class TaiKhoanController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+
+}
